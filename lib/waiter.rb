abd97391f9ddb645fea4ed1a907b7fa269bba0c4
Kevin Downs <kevin.downs04@gmail.com>
2019-11-16 21:08:47 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,30 @@
-class Waiter
 
+class Waiter
+  
+  attr_accessor :name, :years_experience
+  
+  @@all = []
+  
+  def initialize(name, experience)
+    @name = name
+    @years_experience = experience
+    @@all << self
+  end
+  
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+  
+  def meals
+    Meal.all.select {|meal| meal.waiter == self}
+  end
+  
+  def best_tipper
+    
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
 end
